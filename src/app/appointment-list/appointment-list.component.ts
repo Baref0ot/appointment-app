1b47cea0f10159155a142bad1986282d3f8757d4
WRIGHT Matthew L <matthew.wright@hexagon.com>
2025-11-07 03:11:49 -0500

    Refreshing Angular: Initial Commit.

---
+++ src/app/appointment-list/appointment-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-appointment-list',
+  templateUrl: './appointment-list.component.html',
+  styleUrls: ['./appointment-list.component.css']
+})
+
+export class AppointmentListComponent {
+  
+  // property
+  appointment: string = 'Take dog for a walk';
+  
+}
